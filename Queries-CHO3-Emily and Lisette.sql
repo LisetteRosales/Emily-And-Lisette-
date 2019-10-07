be80ae42eacb6f7c5e4a34561bf5ee374783b8bf
Emily Straub <55199567+Estraub624@users.noreply.github.com>
2019-10-07 10:24:26 -0500

    Create Queries-CHO3-Emily and Lisette.sql

---
+++ Queries-CHO3-Emily and Lisette.sql
@@ -0,0 +1,15 @@
+/* *** SQL-Query-CHO3-01- Emily and Lisette *** */ 
+SELECT ProjectID, ProjectName, Department, MaxHours, StartDate, EndDate
+FROM   PROJECT; 
+
+/* *** SQL-Query-CHO3-02- Emily and Lisette *** */
+SELECT * 
+FROM   PROJECT; 
+
+/* *** SQL-Query-CHO3-03- Emily and Lisette *** */
+SELECT ProjectName, Department, MaxHours 
+FROM   PROJECT; 
+
+/* *** SQL-Query-CHO3-04- Emily and Lisette *** */
+SELECT ProjectName, MaxHours, Department 
+From   PROJECT; 
